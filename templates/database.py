7a868aac29c6aae536bd6af502ac41085fa26953
Nikhil <nn327103@gmail.com>
2026-03-20 03:26:36 +0530

    fixed dashboard live update

--- templates/database.py
+++ templates/database.py
@@ -1,10 +1,13 @@
 import sqlite3
 
+DB_NAME = "attendance.db"
+
+
 def init_db():
-    conn = sqlite3.connect('attendance.db')
+    conn = sqlite3.connect(DB_NAME)
     c = conn.cursor()
 
-    c.execute('''
+    c.execute("""
         CREATE TABLE IF NOT EXISTS attendance (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             uid TEXT,
@@ -12,27 +15,32 @@ def init_db():
             time TEXT,
             date TEXT
         )
-    ''')
+    """)
 
     conn.commit()
     conn.close()
+
 
 def insert_record(uid, name, time, date):
-    conn = sqlite3.connect('attendance.db')
+    conn = sqlite3.connect(DB_NAME)
     c = conn.cursor()
 
-    c.execute("INSERT INTO attendance (uid, name, time, date) VALUES (?, ?, ?, ?)",
-              (uid, name, time, date))
+    c.execute(
+        "INSERT INTO attendance (uid, name, time, date) VALUES (?, ?, ?, ?)",
+        (uid, name, time, date)
+    )
 
     conn.commit()
     conn.close()
 
-def get_all():
-    conn = sqlite3.connect('attendance.db')
+
+def get_all_records():
+    conn = sqlite3.connect(DB_NAME)
     c = conn.cursor()
 
-    c.execute("SELECT uid, name, time, date FROM attendance")
-    data = c.fetchall()
+    c.execute("SELECT uid, name, time, date FROM attendance ORDER BY id DESC")
+    rows = c.fetchall()
 
     conn.close()
-    return data
+
+    return rows
