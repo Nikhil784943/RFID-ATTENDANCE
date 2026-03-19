72b2841e9d86642b013b4c34928025f2c660073c
Nikhil <nn327103@gmail.com>
2026-03-20 03:18:27 +0530

    fixed dashboard live update

---
+++ templates/database.py
@@ -0,0 +1,38 @@
+import sqlite3
+
+def init_db():
+    conn = sqlite3.connect('attendance.db')
+    c = conn.cursor()
+
+    c.execute('''
+        CREATE TABLE IF NOT EXISTS attendance (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            uid TEXT,
+            name TEXT,
+            time TEXT,
+            date TEXT
+        )
+    ''')
+
+    conn.commit()
+    conn.close()
+
+def insert_record(uid, name, time, date):
+    conn = sqlite3.connect('attendance.db')
+    c = conn.cursor()
+
+    c.execute("INSERT INTO attendance (uid, name, time, date) VALUES (?, ?, ?, ?)",
+              (uid, name, time, date))
+
+    conn.commit()
+    conn.close()
+
+def get_all():
+    conn = sqlite3.connect('attendance.db')
+    c = conn.cursor()
+
+    c.execute("SELECT uid, name, time, date FROM attendance")
+    data = c.fetchall()
+
+    conn.close()
+    return data
